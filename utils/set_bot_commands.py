5f4c41b045898d1d70f4f392353628b06313a594
Anvarbek <121457366+anvarbeckk@users.noreply.github.com>
2023-12-02 00:00:37 +0500

    fix: update

--- utils/set_bot_commands.py
+++ utils/set_bot_commands.py
@@ -2,13 +2,13 @@ from aiogram import types
 
 
 async def set_default_commands(dp):
-    await dp.bot.delete_my_commands()
-    # await dp.bot.set_my_commands(
-    #     [
-    #         types.BotCommand(command="start", description="about info"),
-    #         types.BotCommand(command="help", description="how to use the bot"),
-    #         types.BotCommand(command="prayer_times", description="seeing prayer times")
-    #     ]
-    # )
+    # await dp.bot.delete_my_commands()
+    await dp.bot.set_my_commands(
+        [
+            types.BotCommand(command="start", description="about info"),
+            types.BotCommand(command="help", description="how to use the bot"),
+            types.BotCommand(command="prayer_times", description="seeing prayer times")
+        ]
+    )
 
-    # await dp.bot.set_my_commands()
+    await dp.bot.set_my_commands()
